d63b1957c6250c2d1937c40e53549f8fadfcbcf1
harshitarana1110 <108141563+harshitarana1110@users.noreply.github.com>
2022-09-24 19:37:14 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,28 @@
+let string = "";
+let buttons = document.querySelectorAll('.button');
+Array.from(buttons).forEach((button)=>{
+    button.addEventListener('click',(e)=>{
+        if(e.target.innerHTML == '='){
+            string = eval(string);
+            document.querySelector('input').value = string;
+        }
+        else if(e.target.innerHTML == 'AC')
+        {
+            string = "";
+            document.querySelector('input').value = string;
+        }
+        else if(e.target.innerHTML == 'C')
+        {
+            string = string.slice(0,-1);
+            document.querySelector('input').value = string;
+        }
+        else{
+            console.log(e.target);
+            if (string =='0')
+            string = e.target.innerHTML;
+            else
+            string = string + e.target.innerHTML;
+            document.querySelector( 'input').value = string;
+        }
+    })
+})
